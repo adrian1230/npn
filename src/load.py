e4a04bb1613957f02c29c9910022a3cfc8066e59
adrian1230 <adrian1145d98@gmail.com>
2021-02-27 01:26:52 +0800

    adrian

--- src/load.py
+++ src/load.py
@@ -17,65 +17,60 @@ headers = {
     'x-rapidapi-host': "aylien-text.p.rapidapi.com"
     }
 
-conn.request("GET", "/extract?url=https://medium.com/sexposblog/good-vibes-130e2eba4cd2", headers=headers)
+def get(link):
+    conn.request("GET", "/extract?url={}".format(link), headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    data = data.decode("utf-8")
+    loc = data[data.find("article"):data.find("article")+len('article')]
+    data = data.split(loc)[1].split('\n')[0][4:].split('.')
+    note = []
+    for t in range(len(data)):
+        e, q = [], []
+        for y in range(len(data[t])):
+            if data[t][y] == '!':
+                e.append(y)
+            elif data[t][y] == '?':
+                q.append(y)
+            else:
+                pass
+        note.append([e,q])
 
-res = conn.getresponse()
-data = res.read()
-
-data = data.decode("utf-8")
-
-loc = data[data.find("article"):data.find("article")+len('article')]
-
-data = data.split(loc)[1].split('\n')[0][4:].split('.')
-
-note = []
-
-for t in range(len(data)):
-    e, q = [], []
-    for y in range(len(data[t])):
-        if data[t][y] == '!':
-            e.append(y)
-        elif data[t][y] == '?':
-            q.append(y)
+    entire = []
+    for c in range(len(note)):
+        if len(note[c][0]) == 0 and len(note[c][1]) == 0:
+            entire.append(data[c])
+        if len(note[c][0]) != 0:
+            for f in range(len(note[c][0])):
+                line = data[c].split(data[c][note[c][0][f]])
+                for b in range(len(line)):
+                    entire.append(line[b])
+        elif len(note[c][1]) != 0:
+            for f in range(len(note[c][1])):
+                line = data[c].split(data[c][note[c][1][f]])
+                for b in range(len(line)):
+                    entire.append(line[b])
+        elif len(note[c][0]) != 0 and len(note[c][1]) != 0:
+            for f in range(len(note[c][0])):
+                line = data[c].split(data[c][note[c][0][f]])
+                for b in range(len(line)):
+                    entire.append(line[b])
+            for f in range(len(note[c][1])):
+                line = data[c].split(data[c][note[c][1][f]])
+                for b in range(len(line)):
+                    entire.append(line[b])
         else:
             pass
-    note.append([e,q])
 
-entire = []
+    alp = 'abcdefghijklmnopqrstuvwxyz'
+    ha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-for c in range(len(note)):
-    if len(note[c][0]) == 0 and len(note[c][1]) == 0:
-        entire.append(data[c])
-    if len(note[c][0]) != 0:
-        for f in range(len(note[c][0])):
-            line = data[c].split(data[c][note[c][0][f]])
-            for b in range(len(line)):
-                entire.append(line[b])
-    elif len(note[c][1]) != 0:
-        for f in range(len(note[c][1])):
-            line = data[c].split(data[c][note[c][1][f]])
-            for b in range(len(line)):
-                entire.append(line[b])
-    elif len(note[c][0]) != 0 and len(note[c][1]) != 0:
-        for f in range(len(note[c][0])):
-            line = data[c].split(data[c][note[c][0][f]])
-            for b in range(len(line)):
-                entire.append(line[b])
-        for f in range(len(note[c][1])):
-            line = data[c].split(data[c][note[c][1][f]])
-            for b in range(len(line)):
-                entire.append(line[b])
-    else:
-        pass
+    for x in range(len(entire)):
+        loca = entire[x].find("\n")
+        print(loca)
 
-alp = 'abcdefghijklmnopqrstuvwxyz'
-ha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for v, m in enumerate(entire):
+        print(v,"=>",m)
+        print("----------------------------")
 
-for x in range(len(entire)):
-    loca = entire[x].find("\n")
-    print(loca)
-
-for v, m in enumerate(entire):
-    print(v,"=>",m)
-    print("----------------------------")
-
+get('https://medium.com/sexposblog/good-vibes-130e2eba4cd2')
