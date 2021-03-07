34a267a7264740913b5b21cb67a3796f4fb560f5
adrian1230 <adrian1145d98@gmail.com>
2021-03-07 22:02:20 +0800

    adrian

--- src/bread.py
+++ src/bread.py
@@ -174,7 +174,7 @@ def extract(point):
                     doc = nlp(formulated)
                     for i in doc:
                         if i.is_stop == False:
-                            print(i,'=>',i.pos_,'=>',i.tag_,'=>',i.dep_,'=>',i.is_alpha,'=>',i.shape_, "=>",i.morph)
+                            print(i,'=>',i.pos_,'=>',i.tag_,'=>',i.dep_,'=>',i.is_alpha,'=>',i.shape_, "=>",i.lemma_)
                     print('\n')
                     print(formulated,'\n')
 extract(test)
