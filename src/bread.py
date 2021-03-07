35be50b842eb683ba54600a1635b1ebb7f7affd7
adrian1230 <adrian1145d98@gmail.com>
2021-03-07 21:45:53 +0800

    adrian

--- src/bread.py
+++ src/bread.py
@@ -173,7 +173,7 @@ def extract(point):
                     formulated = ' '.join(reconstructed)
                     doc = nlp(formulated)
                     for i in doc:
-                        print(i,'=>',i.pos_,'=>',i.dep_,'=>',i.is_alpha,'=>',i.is_stop)
+                        print(i,'=>',i.pos_,'=>',i.tag_,'=>',i.dep_,'=>',i.is_alpha,'=>',i.is_stop)
                     print('\n')
                     print(formulated,'\n')
 extract(test)
