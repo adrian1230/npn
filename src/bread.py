623af68f95854c90f044b6f6d45fd9753188575d
adrian1230 <adrian1145d98@gmail.com>
2021-03-12 21:40:39 +0800

    adrian

--- src/bread.py
+++ src/bread.py
@@ -2,6 +2,7 @@ import spacy as sp
 from bye import *
 from operator import itemgetter
 import numpy as np
+import math as ma
 
 # load the spacy module
 # nlp = sp.load('../model/spacy_finance')
@@ -217,7 +218,13 @@ def extract(point):
                                 c += 1
                     p += 1
                 if c != 0:
-                    print(ner)
+                    ner_text = []
+                    n = 0
+                    while n != len(ner):
+                        for d in range(len(ner[n])):
+                            if ner[n][d] not in labels_:
+                                ner_text.append(ner[n][d])
+                        n += 1
                     kucken = nlp(words)
                     for j in kucken:
                         if j.is_stop == True:
@@ -239,41 +246,144 @@ def extract(point):
                     for k in kucken:
                         if k.is_stop == True:
                             stop_.append(k.text)
-                    coin = words.split(' ')
+                    for f in kucken.ents:
+                        if f.text in stop_:
+                            stop_.remove(f.text)
+                    stop_ = list(set(stop_))
+                    coin = left.split(' ')
+                    for j in range(len(coin)):
+                        if ',' in coin[j]:
+                            coin[j] = coin[j].split(',')[0]
+                        elif '.' in coin[j]:
+                            coin[j] = coin[j].split('.')[0]
+                        elif '?' in coin[j]:
+                            coin[j] = coin[j].split('?')[0]
+                        elif '!' in coin[j]:
+                            coin[j] = coin[j].split('!')[0]
+                        else:
+                            pass
                     g = 0
                     impure = []
                     while g != len(coin):
                         if coin[g] in stop_:
                             impure.append(coin[g])
                         g += 1
-                    impure = set(impure)
-                    coin = [j for j in coin if j not in impure]
-                    words = ' '.join(coin)
-                    never = [
-                            (
-                                sent_,
-                                words,
-                                {
-                                    "combination": [
-                                        main_entity,
-                                        action,
-                                        target_entity,
-                                        date,
-                                    ]
-                                }
-                            )
-                        ]
-                    print(never,'\n')
-                    final.append(
-                        never
-                    )
+                    coin = ' '.join([j for j in coin if j not in set(impure)]).strip()
+                    ########################
+                    cow = sent_.split(',')
+                    for k in range(len(cow)):
+                        cow[k] = cow[k].strip()
+                    jo = cow[0].split()
+                    m = 0
+                    b = 0
+                    length_ner = len(ner_text)
+                    for n in range(len(jo)):
+                        if jo[n] in stop_:
+                           m += 1
+                    for n in range(len(jo)):
+                        if jo[n] in ner_text:
+                           length_ner -= 1
+                    for n in range(len(jo)):
+                        if jo[n] in main_entity:
+                            b += 1
+                        elif jo[n] in target_entity:
+                            b += 1
+                        elif jo[n] in action:
+                            b += 1
+                        else:
+                            pass
+                    med = int(ma.floor(len(jo)/2))
+                    if m <= med or b <= med:
+                        if length_ner == 0:
+                            del cow[0]
+                    w = 0
+                    selection = []
+                    d = 0
+                    while w != (len(cow)-d):
+                        h = 0
+                        r = 0
+                        out = []
+                        beef = cow[w]
+                        for q in range(len(ner_text)):
+                            if ner_text[q] in beef:
+                                out.append(ner_text[q])
+                                beef = ' '.join(cow[w].split(ner_text[q]))
+                        merch = beef.split()
+                        for s in out:
+                            merch.append(s)
+                        print(merch)
+                        height = len(merch)
+                        if height != 2:
+                            wer = 1
+                        if height == 2:
+                            wer = 0
+                        ian = int(ma.floor(height/2)+wer)
+                        for s in range(len(merch)):
+                            if merch[s] in stop_:
+                                print("stop",merch[s])
+                                h += 1
+                        for s in range(len(merch)):
+                            if merch[s] in ner_text:
+                                if merch[s] in main_entity:
+                                    print("ner","subj", merch[s])
+                                    r += 4
+                                elif merch[s] in target_entity:
+                                    print("ner", "obej", merch[s])
+                                    r += 3
+                                else:
+                                    pass
+                            else:
+                                if merch[s] in main_entity:
+                                    print("subj", merch[s])
+                                    r += 2
+                                elif merch[s] in target_entity:
+                                    print("obej", merch[s])
+                                    r += 1
+                                elif merch[s] in action:
+                                    print("verb", merch[s])
+                                    r += 1
+                                else:
+                                    pass
+                        print(h, r, ian, '\n##############')
+                        if h >= ian or r <= ian:
+                            selection.append(cow[w])
+                        w += 1
+                    selection = set(selection)
+                    print(selection)
+                    cows = [j for j in cow if j not in selection]
+                    if length_ner != 0:
+                        print(ner_text)
+                        print(cows)
+                        # dos = nlp(sent_)
+                        never = [
+                                (
+                                    sent_,
+                                    {
+                                        "combination": [
+                                            main_entity,
+                                            action,
+                                            target_entity,
+                                            date,
+                                        ]
+                                    }
+                                )
+                            ]
+                        print(never,'\n')
+                        final.append(
+                            never
+                        )
     return final
 
-bit = "In the domestic business, against a backdrop where the NDP-reported consume electronic categories, which represents approximately 65% of our revenue were down 5.3%, our comparable sales in contracts, excluding the impact of installment billing, declined only 0.7% as we continued to take advantage of strong product cycles in large screen television and iconic mobile phones and confined growth in the appliance category."
+bit = [
+    "In the domestic business, against a backdrop where the NDP-reported consume electronic categories, which represents approximately 65% of our revenue were down 5.3%, our comparable sales in contracts, excluding the impact of installment billing, declined only 0.7% as we continued to take advantage of strong product cycles in large screen television and iconic mobile phones and confined growth in the appliance category.",
+    "The FDA's approval of KYMRIAH as the first immune cell therapy is, of course, a very exciting development for the field of oncology, opening a whole new world of possibility for gene and cell therapies.",
+    # "At Illumina, we strongly believe that sequencing offers the promise to transform lives and are encouraged to see sequencing continue its progress into clinical application.",
+    # "The other driver of course has been the optimization of our merchandising activities, and the resulting impact on gross profit margin."
+]
 
-# wer = extract([book[160],bit])
+wer = extract(bit)
 
-go = np.random.randint(420)
-
-wer = extract(book[go:go+25])
+# go = np.random.randint(420)
+#
+# wer = extract(book[go:go+25])
 
