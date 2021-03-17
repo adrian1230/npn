1d7deb836580af3f94f60ace98cd1faf7615687c
adrian1230 <adrian1145d98@gmail.com>
2021-03-17 22:09:34 +0800

    adrian

---
+++ src/asd.py
@@ -0,0 +1,10 @@
+# For remittances, Visa Direct is one of the only cross border networks
+# that is embedded AML and KYC controls framework and also provides real time in-flight
+# sanction screening information, and clients value this assistance with their compliance efforts.
+# ('V', 'is', 'a Direct')
+#
+# In addition to help customers after their purchase, we’re piloting product support pages, which provide
+# customers information about the products they’ve purchased including transaction details, product
+# specs, Geek Squad plans and quick access to expert help.
+#
+# ('we’re piloting product support pages which provide customers information about the products they', '’ve', 'purchased including transaction details product specs Geek Squad')
