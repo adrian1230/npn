4d5ddd83c8db03c89e8d332f499bb990d84278f8
adrian1230 <47051129+adrian1230@users.noreply.github.com>
2021-04-03 20:26:47 +0800

    by chun cheong so

--- src/vpsmt.py
+++ src/vpsmt.py
@@ -1,8 +1,9 @@
 import spacy as sp
+from nltk.corpus import stopwords
 
 nlp = sp.load('en_core_web_sm')
-
-conjunction_ = ["from","and","or","Or","And","From","Nor","nor","By","by","with","With"]
+stops__ = stopwords.words('english')
+conjunction_ = ["and","or","And","By","by","with","With"]
 
 def extract(_sentence):
     svod_pair = ()
@@ -42,10 +43,8 @@ def extract(_sentence):
     if len(entity_b) != 0:
         ner_text_only = [ner_tags[j][0] for j in range(len(ner_tags))]
         def clean(box):
-            from nltk.corpus import stopwords
-            stops_ = stopwords.words('english')
             for j in box:
-                if j in stops_:
+                if j in stops__:
                     gps = box.index(j)
                     del box[gps]
             box = list(set(box))
@@ -80,8 +79,8 @@ def extract(_sentence):
                 if sent__[j].dep_ == "ROOT" or sent__[j].pos_ == "VERB" or sent__[j].pos_ == "AUX" or sent__[j].dep_ == "advmod" or sent__[j].dep_ == "advcl":
                     verb_only.append(sent__[j].text)
             if len(verb_only) != 0:
-                final_subject_for_svod_pair, final_verb_for_svod_pair, final_object_for_svod_pair = '', '', ''
-                final_date_time_for_svod_pair, _all_combination_of_verb_break_point = [], []
+                final_subj, final_verb, final_obj = '', '', ''
+                final_dt, _all_combination_of_verb_break_point = [], []
                 # A B C D 4 verbs => (D, CD, BCD, ABCD) only
                 _all_combination_of_verb_break_point.append(verb_only[-1])
                 while len(_all_combination_of_verb_break_point) != len(verb_only):
@@ -105,52 +104,52 @@ def extract(_sentence):
                 delete_combination = [_all_combination_of_verb_break_point[d] for d in range(len(rating_of_all_verb_combination)) if rating_of_all_verb_combination[d] != 0]
                 _all_combination_of_verb_break_point = [f for f in _all_combination_of_verb_break_point if f not in delete_combination or f != '']
                 if len(_all_combination_of_verb_break_point) == 0:
-                    final_subject_for_svod_pair = extracted_core_sent_string.split(verb_only[len(verb_only) - 1])[0].strip()
-                    final_object_for_svod_pair = extracted_core_sent_string.split(verb_only[len(verb_only) - 1])[1].strip()
-                    final_verb_for_svod_pair = verb_only[len(verb_only) - 1].strip()
+                    final_subj = extracted_core_sent_string.split(verb_only[len(verb_only) - 1])[0].strip()
+                    final_obj = extracted_core_sent_string.split(verb_only[len(verb_only) - 1])[1].strip()
+                    final_verb = verb_only[len(verb_only) - 1].strip()
                 if len(_all_combination_of_verb_break_point) != 0:
-                    final_subject_for_svod_pair = extracted_core_sent_string.split(_all_combination_of_verb_break_point[-1])[0].strip()
-                    final_object_for_svod_pair = extracted_core_sent_string.split(_all_combination_of_verb_break_point[-1])[1].strip()
-                    final_verb_for_svod_pair = _all_combination_of_verb_break_point[-1].strip()
+                    final_subj = extracted_core_sent_string.split(_all_combination_of_verb_break_point[-1])[0].strip()
+                    final_obj = extracted_core_sent_string.split(_all_combination_of_verb_break_point[-1])[1].strip()
+                    final_verb = _all_combination_of_verb_break_point[-1].strip()
                 for g in range(len(dt_b)):
                     chunk = dt_b[g]
                     counter_of_chunk = dt_b.count(dt_b[g])
-                    if chunk in final_subject_for_svod_pair:
+                    if chunk in final_subj:
                         if counter_of_chunk <= 1:
-                            final_subject_for_svod_pair = ''.join(final_subject_for_svod_pair.split(chunk))
-                            final_date_time_for_svod_pair.append(''.join([dt_b[g],'']))
+                            final_subj = ''.join(final_subj.split(chunk))
+                            final_dt.append(''.join([dt_b[g],'']))
                         if counter_of_chunk >= 2:
                             all_recurring_box = []
                             f = 0
                             while f != counter_of_chunk:
                                 if f == 0:
-                                    all_recurring_box.append(final_subject_for_svod_pair.index(chunk))
+                                    all_recurring_box.append(final_subj.index(chunk))
                                 else:
-                                    all_recurring_box.append(final_subject_for_svod_pair.index(chunk,all_recurring_box[-1]+f))
+                                    all_recurring_box.append(final_subj.index(chunk,all_recurring_box[-1]+f))
                                 f += 1
-                            filtered_part = final_subject_for_svod_pair[all_recurring_box[0]:all_recurring_box[-1]+len(dt_b[g])]
-                            final_subject_for_svod_pair = ''.join(final_subject_for_svod_pair.split(filtered_part))
-                            final_date_time_for_svod_pair.append(''.join([filtered_part,'']))
-                    elif chunk in final_object_for_svod_pair:
+                            filtered_part = final_subj[all_recurring_box[0]:all_recurring_box[-1]+len(dt_b[g])]
+                            final_subj = ''.join(final_subj.split(filtered_part))
+                            final_dt.append(''.join([filtered_part,'']))
+                    elif chunk in final_obj:
                         if counter_of_chunk <= 1:
-                            final_object_for_svod_pair = ''.join(final_object_for_svod_pair.split(chunk))
-                            final_date_time_for_svod_pair.append(''.join([dt_b[g],'']))
+                            final_obj = ''.join(final_obj.split(chunk))
+                            final_dt.append(''.join([dt_b[g],'']))
                         if counter_of_chunk >= 2:
                             all_recurring_box = []
                             f = 0
                             while f != counter_of_chunk:
                                 if f == 0:
-                                    all_recurring_box.append(final_object_for_svod_pair.index(chunk))
+                                    all_recurring_box.append(final_obj.index(chunk))
                                 else:
                                     all_recurring_box.append(
-                                        final_object_for_svod_pair.index(chunk, all_recurring_box[-1] + f))
+                                        final_obj.index(chunk, all_recurring_box[-1] + f))
                                 f += 1
-                            filtered_part = final_object_for_svod_pair[all_recurring_box[0]:all_recurring_box[-1] + len(dt_b[g])]
-                            final_object_for_svod_pair = ''.join(final_object_for_svod_pair.split(filtered_part))
-                            final_date_time_for_svod_pair.append(''.join([filtered_part,'']))
+                            filtered_part = final_obj[all_recurring_box[0]:all_recurring_box[-1] + len(dt_b[g])]
+                            final_obj = ''.join(final_obj.split(filtered_part))
+                            final_dt.append(''.join([filtered_part,'']))
                     else:
                         pass
-                final_subj_split = final_subject_for_svod_pair.split()
+                final_subj_split = final_subj.split()
                 if len(final_subj_split) > 1:
                     rate_subj = []
                     def rating_(_pair,ent,c):
@@ -179,7 +178,7 @@ def extract(_sentence):
                             final_subj_split = final_subj_split[chunk_in_subject_for_removal_1:chunk_in_subject_for_removal_1+1]
                         if chunk_in_subject_for_removal != (len(rate_subj)-1):
                             final_subj_split = final_subj_split[chunk_in_subject_for_removal+1:]
-                svod_pair = (' '.join(final_subj_split).strip(),final_verb_for_svod_pair,final_object_for_svod_pair.strip(),' '.join(final_date_time_for_svod_pair).strip())
+                svod_pair = (' '.join(final_subj_split).strip(),final_verb,final_obj.strip(),' '.join(final_dt).strip())
     return svod_pair
 
 if __name__ == '__main__':
